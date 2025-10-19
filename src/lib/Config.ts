e6e0f3f8b991acead7e20ae9453af32401944668
Tom Matheussen <tmatheussen@avia-gis.com>
2025-10-19 17:47:33 +0000

    Some formatting fixes

--- src/lib/Config.ts
+++ src/lib/Config.ts
@@ -1,3 +1,3 @@
-export const APP_TITLE = "GMM Atlas";
-export const APP_TITLE_FULL = "Graspop Metal Meeting Atlas";
-export const APP_DESCRIPTION = "Explore and compare Graspop Metal Meeting maps across the years.";
+export const APP_TITLE = 'GMM Atlas';
+export const APP_TITLE_FULL = 'Graspop Metal Meeting Atlas';
+export const APP_DESCRIPTION = 'Explore and compare Graspop Metal Meeting maps across the years.';
